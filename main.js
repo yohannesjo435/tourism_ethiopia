1473f140504bbc8ad506641739dbe6b314bdee1f
yohannes <yohannesgoat@gmail.com>
2025-05-28 17:51:21 -0300

    added basic hero

---
+++ main.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+let slides = document.getElementsByClassName("slide");
+showSlides(slideIndex)
+slideShow() 
+
+function plusSlide(n) {
+  showSlides(slideIndex += n)
+}
+
+function showSlides(n){
+  if (n > slides.length) {
+    slideIndex = 1
+  }  
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (let i = 0; i < slides.length; i++){
+    slides[i].style.display = "none"
+  }
+  
+  slides[slideIndex -1].style.display = "block"
+}
+
+function slideShow() {
+  if(slideIndex < slides.length){
+    setInterval(()=> {
+        slideIndex++
+        showSlides(slideIndex)
+    }, 5000)
+  }
+}
