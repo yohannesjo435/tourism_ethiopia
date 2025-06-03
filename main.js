2493d744ded246f7340f31bb7495260abfe21faf
yohannes <yohannesgoat@gmail.com>
2025-06-03 07:17:29 -0300

    updated image src atrributes and file imports

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const indexRoute = require("./routes/indexRoute")
 const galleryRoute = require("./routes/galleryRoute")
 const aboutusRouter = require("./routes/aboutusRoute")
 const app = express()
-const PORT = 8080 || 8080
+const PORT = process.env.PORT || 8080
 
-app.set("views", __dirname + "/views")
 app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
 app.use(express.static("public"))
 
 app.use("/", indexRoute)
